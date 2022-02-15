09787b65fefb668d24cb34e9d6702690268f903e
DongHyeok Lim <zx07050@naver.com>
2022-02-15 20:51:11 +0900

    pomodoro initial commit
    기본 UI 구성

---
+++ Project 06 - Pomodoro/pomodoro/pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pomodoro
+//
+//  Created by 임동혁 on 2022/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
