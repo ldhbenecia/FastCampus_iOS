c1ec5f1a7790633cd0b1bd7f1d2cd327988cdb47
DongHyeok Lim <zx07050@naver.com>
2022-02-17 19:04:54 +0900

    [feat] UILabel 기능 구현

--- Project 06 - Pomodoro/pomodoro/pomodoro/ViewController.swift
+++ Project 06 - Pomodoro/pomodoro/pomodoro/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progreeView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+    
+    var duration = 60
+    var timerStatus: TimerStatus = .end // Timer의 상태, enum에 열거되어있음
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureToggleButton()
+    }
+    
+    func setTimerInfoViewVisble(isHidden: Bool) {
+        self.timerLabel.isHidden = isHidden
+        self.progreeView.isHidden = isHidden
+    }
+    
+    // 시작버튼을 눌렀을 때 일시정지가 되도록 구현
+    func configureToggleButton() {
+        self.toggleButton.setTitle("시작", for: .normal)
+        self.toggleButton.setTitle("일시정지", for: .selected)
     }
 
-
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        switch self.timerStatus {
+        case .start, .pause:
+            self.timerStatus = .end
+            self.cancelButton.isEnabled = false //취소버튼 비활성화
+            self.setTimerInfoViewVisble(isHidden: true) // TimerLabel, progreeView 표시 x
+            self.datePicker.isHidden = false
+            self.toggleButton.isSelected = false //시작
+        
+        default:
+            break
+        }
+    }
+    
+    @IBAction func tapToggleButton(_ sender: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration) // datePicker에서 설정한 타이머 시간을 초로 가져올 수 있음
+        switch self.timerStatus {
+        case .end:
+            self.timerStatus = .start
+            self.setTimerInfoViewVisble(isHidden: false) // timerLabel, progreeView 나타남
+            self.datePicker.isHidden = true
+            self.toggleButton.isSelected = true
+            self.cancelButton.isEnabled = true
+            
+        case .start:
+            self.timerStatus = .pause
+            self.toggleButton.isSelected = false //시작
+        
+        case .pause:
+            self.timerStatus = .start
+            self.toggleButton.isSelected = true //일시정지
+        }
+    }
 }
 
