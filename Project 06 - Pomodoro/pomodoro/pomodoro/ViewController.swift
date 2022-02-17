56b7f6a90507016682d68b5a38dacf0ccc2b957e
DongHyeok Lim <zx07050@naver.com>
2022-02-17 20:20:06 +0900

    [feat] timer 작동 구현

--- Project 06 - Pomodoro/pomodoro/pomodoro/ViewController.swift
+++ Project 06 - Pomodoro/pomodoro/pomodoro/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
     
     var duration = 60
     var timerStatus: TimerStatus = .end // Timer의 상태, enum에 열거되어있음
+    var timer: DispatchSourceTimer?
+    var currentSeconds = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,15 +42,41 @@ class ViewController: UIViewController {
         self.toggleButton.setTitle("시작", for: .normal)
         self.toggleButton.setTitle("일시정지", for: .selected)
     }
+    
+    func startTimer() {
+        if self.timer == nil {
+            self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            self.timer?.schedule(deadline: .now(), repeating: 1)
+            self.timer?.setEventHandler(handler: { [weak self] in
+                self?.currentSeconds -= 1
+                debugPrint(self?.currentSeconds)
+                
+                if self?.currentSeconds ?? 0 <= 0 {
+                    self?.startTimer()
+                    // 타이머가 종료
+                }
+            })
+            self.timer?.resume()
+        }
+    }
+    
+    func stopTimer() {
+        if self.timerStatus == .pause {
+            self.timer?.resume()
+        }
+        self.timerStatus = .end
+        self.cancelButton.isEnabled = false //취소버튼 비활성화
+        self.setTimerInfoViewVisble(isHidden: true) // TimerLabel, progreeView 표시 x
+        self.datePicker.isHidden = false
+        self.toggleButton.isSelected = false //시작
+        self.timer?.cancel()
+        self.timer = nil
+    }
 
     @IBAction func tapCancelButton(_ sender: UIButton) {
         switch self.timerStatus {
         case .start, .pause:
-            self.timerStatus = .end
-            self.cancelButton.isEnabled = false //취소버튼 비활성화
-            self.setTimerInfoViewVisble(isHidden: true) // TimerLabel, progreeView 표시 x
-            self.datePicker.isHidden = false
-            self.toggleButton.isSelected = false //시작
+            self.stopTimer()
         
         default:
             break
@@ -59,19 +87,23 @@ class ViewController: UIViewController {
         self.duration = Int(self.datePicker.countDownDuration) // datePicker에서 설정한 타이머 시간을 초로 가져올 수 있음
         switch self.timerStatus {
         case .end:
+            self.currentSeconds = self.duration
             self.timerStatus = .start
             self.setTimerInfoViewVisble(isHidden: false) // timerLabel, progreeView 나타남
             self.datePicker.isHidden = true
             self.toggleButton.isSelected = true
             self.cancelButton.isEnabled = true
+            self.startTimer()
             
         case .start:
             self.timerStatus = .pause
             self.toggleButton.isSelected = false //시작
+            self.timer?.suspend() // 타이머 일시정지
         
         case .pause:
             self.timerStatus = .start
             self.toggleButton.isSelected = true //일시정지
+            self.timer?.resume() // 타이머 재시작
         }
     }
 }
