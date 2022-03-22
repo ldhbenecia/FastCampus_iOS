32caf3dc51dbd9702ae24024229b916b051cf936
DongHyeok Lim <zx07050@naver.com>
2022-03-22 17:50:56 +0900

    Covid Ui initial Commit

---
+++ Project 08 - Covid19/COVID19/COVID19/CovidDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19
+//
+//  Created by 임동혁 on 2022/03/22.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
---
+++ Project 08 - Covid19/COVID19/COVID19/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 임동혁 on 2022/03/22.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var pieChartsView: PieChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
