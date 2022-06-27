cbd13e6083005b2560a7816e01bfa9d1a3d43a19
DongHyeok Lim <zx07050@naver.com>
2022-06-27 17:53:09 +0900

    [Feat] 이메일, 비밀번호 로그인 구현

--- Project 09 - SpotifyLoginSampleApp/SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmialViewController.swift
+++ Project 09 - SpotifyLoginSampleApp/SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmialViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EnterEmailViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -35,7 +36,46 @@ class EnterEmailViewController: UIViewController {
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        //Firebase 이메일/비밀번호 인증
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
         
+        //신규 사용자 생성
+        Auth.auth().createUser(withEmail: email, password: password) {[weak self] authResult, error in guard let self = self else { return }
+            
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007: //이미 가입한 계정일 때
+                    //로그인하기
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    // 또 다른 에러의 경우 에러메세지 출력
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            } else {
+                //모든 에러가 없을 때는 메인 화면으로 이동
+                self.showMainViewController()
+            }
+        }
+    }
+    
+    private func showMainViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        navigationController?.show(mainViewController, sender: nil)
+    }
+    
+    private func loginUser(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] _, error in guard let self = self else { return }
+            
+            if let error = error {
+                self.errorMessageLabel.text = error.localizedDescription
+            } else {
+                self.showMainViewController()
+            }
+        }
     }
 }
 
